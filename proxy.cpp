b0fe36bc52cf462f3b8f2aa03118a6f8809429dd
Cody Pitts <cody.pitts@gmail.com>
2016-04-27 20:44:47 -0700

    trying to add more socket functionality

--- proxy.cpp
+++ proxy.cpp
@@ -23,12 +23,13 @@ using namespace std;
 #define BACKLOG 10 //how many pending connections queue will hold
 
 #define MAXDATASIZE 1000
-const int MAXARGUMENTS = 1000;
+const int MAXARGUMENTS = 20000;
+const char* port = "10347";
 
-string absoluteToRelative(string absolute_uri, int &server_port_num);
+string absoluteToRelative(string absolute_uri, int &server_port_num, string &hostname);
 
 string parseClientArguments(string unparsed_message,
-                                    string delimiter, int &server_port_num);
+                                    string delimiter, int &server_port_num, string &hostname);
 
 //some functionality from Beej
 void sigchld_handler(int s);
@@ -47,6 +48,8 @@ struct thread_args
   int* thread_size_ptr;
   int proxy_port_num;
   int comm_sock_num;
+  struct addrinfo hints;
+  struct addrinfo servinfo;
 
 };
 
@@ -86,7 +89,7 @@ int main(int argc, char *argv[])
 
   
 
-  if ((rv = getaddrinfo(NULL, "cs2.seattleu.edu", &hints, &servinfo)) != 0) {
+  if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
     return 1;
   }
@@ -160,6 +163,8 @@ int main(int argc, char *argv[])
   (*t_args).thread_size_ptr = &current_size;
   (*t_args).proxy_port_num = 80;
   (*t_args).comm_sock_num = comm_sock;
+  (*t_args).hints = hints;
+  (*t_args).servinfo = servinfo;
 
   pthread_t current_thread = thread_pool[current_size];
   pthread_create(&current_thread, NULL,
@@ -208,10 +213,16 @@ void* threadFunc(void* t_args)
 {
   struct thread_args *passed_args = (struct thread_args*) t_args;
   string unparsed_message;
+  struct addrinfo *p;
   int server_port_num;
+  int client_sock;
+  string hostname;
   int byte_read;
+  int byte_sent;
+  int rv;
   char buffer[MAXDATASIZE];
   char* bp = buffer;
+  string temp_len_1, temp_len_2;
 
   for(int i = 0; i < MAXDATASIZE; i++)
     buffer[i] = '\0';
@@ -224,19 +235,60 @@ void* threadFunc(void* t_args)
 
   unparsed_message = string(bp);
 
-	string parsed_input = parseClientArguments(unparsed_message, "\r\n", server_port_num);
-	string ret;
+	string parsed_input = parseClientArguments(unparsed_message, "\r\n", server_port_num, hostname);
 	//now we need to send and receive
+
+  if((rv = getaddrinfo(hostname.c_str(), server_port_num, &(*passed_args).hints,
+    &(*passed_args).servinfo)) != 0){
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return;
+  }
+
+  for(p = (*passed_args).servinfo; p != NULL; p = p->ai_next) {
+    if ((client_sock = socket(p->ai_family, p->ai_socktype,
+                         p->ai_protocol)) == -1) {
+      cerr << "Socket";
+      continue;
+    }
+
+    if (connect(client_sock, p->ai_addr, p->ai_addrlen) == -1) {
+      close(client_sock);
+      cerr << "Connect";
+      continue;
+    }
+
+    break;
+  }
+
+  if (p == NULL) {
+    fprintf(stderr, "Failed to connect\n");
+    return;
+  }
+
+  do{
+    byte_sent += send(server_port_num, (void*) parsed_input.c_str(), parsed_input.length(), 0);
+  }while(byte_sent > 0 && byte_sent != (int) parsed_input.length());
+
+  while(true){
+    byte_read = recv(server_port_num, (void*)bp, MAXDATASIZE, 0);
+    if(byte_read == 0)
+      break;
+  }
+
+  do{
+    byte_sent += send((*passed_args).comm_sock_num, (void*) buffer, MAXDATASIZE, 0);
+  }while(byte_sent > 0 && byte_sent != (int) MAXDATASIZE);
+
+
 }
 
-string absoluteToRelative(string absolute_uri, int &server_port_num){
+string absoluteToRelative(string absolute_uri, int &server_port_num, string &hostname){
 
   stringstream ssin(absolute_uri);
   int count = 0;
   string chunks[3];
   string unparsed_url;
   string temp_url;
-  string hostname;
   string path = "/";
   string complete;
   size_t host_start_ind;
@@ -273,7 +325,7 @@ string absoluteToRelative(string absolute_uri, int &server_port_num){
 }
 
 string parseClientArguments(string unparsed_message,
-                                    string delimiter, int &server_port_num){
+                                    string delimiter, int &server_port_num, string &hostname){
 
   size_t index = 0;
   string temp;
@@ -287,7 +339,7 @@ string parseClientArguments(string unparsed_message,
       unparsed_message.erase(0, index + delimiter.length());
   }
 
-  arg_lines[0] = absoluteToRelative(arg_lines[0], server_port_num);
+  arg_lines[0] = absoluteToRelative(arg_lines[0], server_port_num, hostname);
 
   for(unsigned int i = 0; i < arg_lines.size(); i++){
     if(arg_lines[i].find("Connection:") != string::npos){
